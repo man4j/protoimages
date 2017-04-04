9080a84352ac991b9c3f221f57739efd688c136e
Vladimir Petrukhin <man4j@ya.ru>
2017-04-04 11:21:36 +0300

    small fixes

---
+++ monitoring/run_loading_test.sh
@@ -0,0 +1,8 @@
+﻿mysqlslap --concurrency=50 --iterations=1 \
+--auto-generate-sql \
+--auto-generate-sql-guid-primary \
+--auto-generate-sql-secondary-indexes=10 \
+--auto-generate-sql-execute-number=10000 \
+--auto-generate-sql-unique-query-number=50 \
+--auto-generate-sql-unique-write-number=10 \
+--auto-generate-sql-write-number=1000
--- monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
+++ monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
@@ -1,9 +1,9 @@
 package org.protobeans.monitoring.javaagent;
 
 import org.protobeans.core.EntryPoint;
-import org.protobeans.monitoring.annotation.EnableGaleraMonitoring;
+import org.protobeans.monitoring.annotation.EnableMySQLMonitoring;
 
-@EnableGaleraMonitoring(dnsAddr = "s:MYSQL_DNS_ADDRS", rootPassword = "s:MYSQL_ROOT_PASSWORD")
+@EnableMySQLMonitoring(dnsAddr = "s:MYSQL_DNS_ADDRS", rootPassword = "s:MYSQL_ROOT_PASSWORD")
 public class Main {
     public static void main(String[] args) {
         EntryPoint.run(Main.class);
