946c5e7aa4491c40a6aba3f3427d2c91674b8b55
Vladimir Petrukhin <man4j@ya.ru>
2017-04-06 11:32:05 +0300

    haproxy agent fix

--- monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
+++ monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
@@ -1,9 +1,9 @@
 package org.protobeans.monitoring.javaagent;
 
 import org.protobeans.core.EntryPoint;
-import org.protobeans.monitoring.annotation.EnableMySQLMonitoring;
+import org.protobeans.monitoring.annotation.EnableMonitoring;
 
-@EnableMySQLMonitoring(dnsAddr = "s:MYSQL_DNS_ADDRS", rootPassword = "s:MYSQL_ROOT_PASSWORD")
+@EnableMonitoring(dnsAddr = "haproxy:104.236.104.13:14567")
 public class Main {
     public static void main(String[] args) {
         EntryPoint.run(Main.class);
