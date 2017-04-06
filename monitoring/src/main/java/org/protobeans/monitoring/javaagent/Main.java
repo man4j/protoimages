0ac64ee9c3505038c13838cdedced30c09d589e8
Vladimir Petrukhin <man4j@ya.ru>
2017-04-06 12:10:49 +0300

    fix

--- monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
+++ monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
@@ -3,7 +3,7 @@ package org.protobeans.monitoring.javaagent;
 import org.protobeans.core.EntryPoint;
 import org.protobeans.monitoring.annotation.EnableMonitoring;
 
-@EnableMonitoring(dnsAddr = "haproxy:104.236.104.13:14567")
+@EnableMonitoring(dnsAddr = "s:DNS_ADDRS")
 public class Main {
     public static void main(String[] args) {
         EntryPoint.run(Main.class);
