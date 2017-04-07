dd1fd2ca1b95d6bd0905c472eabf95f6f491327c
Vladimir Petrukhin <man4j@ya.ru>
2017-04-07 21:44:06 +0300

    small fixes

--- monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
+++ monitoring/src/main/java/org/protobeans/monitoring/javaagent/Main.java
@@ -1,9 +1,11 @@
 package org.protobeans.monitoring.javaagent;
 
 import org.protobeans.core.EntryPoint;
-import org.protobeans.monitoring.annotation.EnableMonitoring;
+import org.protobeans.monitoring.annotation.EnableDockerMonitoring;
+import org.protobeans.monitoring.annotation.EnableServiceMonitoring;
 
-@EnableMonitoring(dnsAddr = "s:DNS_ADDRS")
+@EnableServiceMonitoring(dnsAddr = "s:DNS_ADDRS")
+@EnableDockerMonitoring
 public class Main {
     public static void main(String[] args) {
         EntryPoint.run(Main.class);
