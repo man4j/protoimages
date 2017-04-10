24521e568b081b62ad20f6be0188878e43ae6198
root <root@docker-4gb-nyc1-02.localdomain>
2017-04-10 12:05:48 +0000

    fix metricbeat

--- mellk/metricbeat/build.sh
+++
@@ -1,2 +0,0 @@
-docker build --no-cache -t man4j/metricbeat:5.3.0_4 .
-docker push man4j/metricbeat:5.3.0_4
---
+++ mellk/start_elasticsearch_kibana.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Starting elasticsearch..."
+docker run --restart=always -d --network monitoring -p 9200:9200 --name elasticsearch \
+--cap-add=IPC_LOCK --ulimit memlock=-1:-1 --ulimit nofile=65536:65536 --memory="1g" --memory-swap="1g" --memory-swappiness=0 \
+-e "http.host=0.0.0.0" \
+-e "transport.host=127.0.0.1" \
+-e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+-e "bootstrap.memory_lock=true" \
+-e "LOGSPOUT=ignore" \
+docker.elastic.co/elasticsearch/elasticsearch:5.3.0
+
+echo "Waiting 180s..."
+sleep 180
+
+echo "Starting kibana..."
+docker run --restart=always -d --network monitoring -p 5601:5601 --name kibana \
+-e "ELASTICSEARCH_URL=http://elasticsearch:9200" \
+-e "ELASTICSEARCH_PASSWORD=PassWord123" \
+docker.elastic.co/kibana/kibana:5.3.0
