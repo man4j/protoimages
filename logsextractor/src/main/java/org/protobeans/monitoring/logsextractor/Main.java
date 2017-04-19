1b2187a159708bc7f51610aa2072de39ca54f8c1
Vladimir Petrukhin <man4j@ya.ru>
2017-04-19 14:03:50 +0300

    small fixes

--- logsextractor/src/main/java/org/protobeans/monitoring/logsextractor/Main.java
+++ logsextractor/src/main/java/org/protobeans/monitoring/logsextractor/Main.java
@@ -2,9 +2,23 @@ package org.protobeans.monitoring.logsextractor;
 
 import org.protobeans.core.EntryPoint;
 import org.protobeans.monitoring.annotation.EnableLogsExtractor;
+import org.protobeans.monitoring.service.logsextractor.FillMetadataProcessingStage;
+import org.protobeans.monitoring.service.logsextractor.JsonLineProcessingStage;
+import org.protobeans.monitoring.service.logsextractor.LogOutputProcessingStage;
+import org.protobeans.monitoring.service.logsextractor.LogProcessingChain;
+import org.protobeans.monitoring.service.logsextractor.MultilineProcessingStage;
+import org.springframework.context.annotation.Bean;
 
 @EnableLogsExtractor(patterns = "s:patterns")
 public class Main {
+    @Bean
+    public LogProcessingChain logProcessingChain() {
+        return new LogProcessingChain().add(new MultilineProcessingStage(" ", "}"))
+                                       .add(new FillMetadataProcessingStage())
+                                       .add(new JsonLineProcessingStage())
+                                       .add(new LogOutputProcessingStage());
+    }
+    
     public static void main(String[] args) {
         EntryPoint.run(Main.class);
     }
