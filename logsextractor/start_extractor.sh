8f829d4253912c6d8c0da69f935df0d0686c4e3e
root <root@docker-4gb-nyc1-02.localdomain>
2017-04-20 12:04:52 +0000

    fix logsextractor

--- logsextractor/start_extractor.sh
+++ logsextractor/start_extractor.sh
@@ -4,4 +4,4 @@ set -e
 docker service create --network monitoring --mode global --name logsextractor --log-opt max-size=10m --log-opt max-file=10 \
 --mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" \
 -e "PATTERNS=galera" \
-man4j/logsextractor:v1
+man4j/logsextractor:v2
